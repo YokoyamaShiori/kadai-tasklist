a0a213d4a6eb2afe05a16b6ba2139c70cce08c6f
shiori.yokoyama <b.dry.doll@gmail.com>
2021-07-16 14:19:25 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to root_url
+      redirect_to new_session_path
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
