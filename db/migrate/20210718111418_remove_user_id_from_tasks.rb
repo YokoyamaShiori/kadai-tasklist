8131013daa16c5f5013e415d2602ff2c708c595b
shiori.yokoyama <b.dry.doll@gmail.com>
2021-07-19 01:50:24 +0000

    for heroku

--- db/migrate/20210718111418_remove_user_id_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromTasks < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :tasks, :UserId, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_111418) do
+ActiveRecord::Schema.define(version: 2021_07_18_040648) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
